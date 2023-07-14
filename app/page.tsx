db0fce73cfe469b27b6b4a9ea50fc3d7d309d456
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-07-14 16:53:06 +0100

    Made some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Header from "./components/heading/heading"
 import HeadingHero from "./components/hero/headingHero"
 import HowItwWokrsHero from "./components/hero/howItWorksHero"
+import CallToAction from "./components/hero/callToActionHero"
+import Footer from "./components/footer/footer"
 
 export default function Home() {
   return (
@@ -12,6 +14,10 @@ export default function Home() {
       <HeadingHero />
 
       <HowItwWokrsHero />
+
+      <CallToAction />
+
+      <Footer />
     
     </>
 
