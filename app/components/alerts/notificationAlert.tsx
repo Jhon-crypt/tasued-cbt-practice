c06bc7663692b8675b67c25020cbb53fc704a51d
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-07-25 15:27:30 +0100

    Added the settings page and components

---
+++ app/components/alerts/notificationAlert.tsx
@@ -0,0 +1,95 @@
+export default function NotificationAlert(){
+
+    return (
+
+        <>
+        
+            <div className="grid grid-flow-row auto-rows-max">
+
+                <div className="w-full p-3 mt-4 bg-white rounded shadow flex flex-shrink-0">
+                    <div aria-label="group icon" role="img" className="focus:outline-none w-8 h-8 border rounded-full border-gray-200 flex flex-shrink-0 items-center justify-center">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.66674 10.1147L12.7947 3.98599L13.7381 4.92866L6.66674 12L2.42407 7.75733L3.36674 6.81466L6.66674 10.1147Z" fill="#047857" />
+                        </svg>
+                    </div>
+                    <div className="pl-3 w-full">
+                        <div className="flex items-center justify-between w-full">
+                            <p className="focus:outline-none text-sm leading-none"><span className="text-indigo-700">150 students</span> took your <span className="text-indigo-700">EDU221</span> test</p>
+                            <div aria-label="close icon" role="button" className="focus:outline-none cursor-pointer">
+                                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.5 3.5L3.5 10.5" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M3.5 3.5L10.5 10.5" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </div>
+                        </div>
+                        <p className="focus:outline-none text-xs leading-3 pt-1 text-gray-500">2 hours ago</p>
+                    </div>
+                </div>
+
+                <div className="w-full p-3 mt-4 bg-white rounded shadow flex flex-shrink-0">
+                    <div aria-label="group icon" role="img" className="focus:outline-none w-8 h-8 border rounded-full border-gray-200 flex flex-shrink-0 items-center justify-center">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.66674 10.1147L12.7947 3.98599L13.7381 4.92866L6.66674 12L2.42407 7.75733L3.36674 6.81466L6.66674 10.1147Z" fill="#047857" />
+                        </svg>
+                    </div>
+                    <div className="pl-3 w-full">
+                        <div className="flex items-center justify-between w-full">
+                            <p className="focus:outline-none text-sm leading-none"><span className="text-indigo-700">150 students</span> took your <span className="text-indigo-700">EDU221</span> test</p>
+                            <div aria-label="close icon" role="button" className="focus:outline-none cursor-pointer">
+                                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.5 3.5L3.5 10.5" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M3.5 3.5L10.5 10.5" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </div>
+                        </div>
+                        <p className="focus:outline-none text-xs leading-3 pt-1 text-gray-500">2 hours ago</p>
+                    </div>
+                </div>
+
+                <div className="w-full p-3 mt-4 bg-white rounded shadow flex flex-shrink-0">
+                    <div aria-label="group icon" role="img" className="focus:outline-none w-8 h-8 border rounded-full border-gray-200 flex flex-shrink-0 items-center justify-center">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.66674 10.1147L12.7947 3.98599L13.7381 4.92866L6.66674 12L2.42407 7.75733L3.36674 6.81466L6.66674 10.1147Z" fill="#047857" />
+                        </svg>
+                    </div>
+                    <div className="pl-3 w-full">
+                        <div className="flex items-center justify-between w-full">
+                            <p className="focus:outline-none text-sm leading-none"><span className="text-indigo-700">150 students</span> took your <span className="text-indigo-700">EDU221</span> test</p>
+                            <div aria-label="close icon" role="button" className="focus:outline-none cursor-pointer">
+                                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.5 3.5L3.5 10.5" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M3.5 3.5L10.5 10.5" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </div>
+                        </div>
+                        <p className="focus:outline-none text-xs leading-3 pt-1 text-gray-500">2 hours ago</p>
+                    </div>
+                </div>
+
+                <div className="w-full p-3 mt-4 bg-white rounded shadow flex flex-shrink-0">
+                    <div aria-label="group icon" role="img" className="focus:outline-none w-8 h-8 border rounded-full border-gray-200 flex flex-shrink-0 items-center justify-center">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.66674 10.1147L12.7947 3.98599L13.7381 4.92866L6.66674 12L2.42407 7.75733L3.36674 6.81466L6.66674 10.1147Z" fill="#047857" />
+                        </svg>
+                    </div>
+                    <div className="pl-3 w-full">
+                        <div className="flex items-center justify-between w-full">
+                            <p className="focus:outline-none text-sm leading-none"><span className="text-indigo-700">150 students</span> took your <span className="text-indigo-700">EDU221</span> test</p>
+                            <div aria-label="close icon" role="button" className="focus:outline-none cursor-pointer">
+                                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.5 3.5L3.5 10.5" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M3.5 3.5L10.5 10.5" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </div>
+                        </div>
+                        <p className="focus:outline-none text-xs leading-3 pt-1 text-gray-500">2 hours ago</p>
+                    </div>
+                </div>
+
+            </div>
+        
+        </>
+
+    )
+
+}
--- app/components/section/settingsSection.tsx
+++ app/components/section/settingsSection.tsx
@@ -1,6 +1,4 @@
-import NotificationAlert from "../alerts/notificationAlert"
-
-export default function NotificationSection(){
+export default function SetttingsSection(){
 
     return (
 
@@ -9,14 +7,8 @@ export default function NotificationSection(){
             <div className="px-6 pt-6 2xl:container">
 
                 <h2 className="mt-10 mb-16 text-4xl md:text-5xl font-bold font-heading text-center tracking-px-n leading-tight">
-                    Notifications
+                    Settings
                 </h2>
-
-                <div className="flex justify-center mt-8">
-
-                    <NotificationAlert />
-
-                </div>
 
             </div>
         
