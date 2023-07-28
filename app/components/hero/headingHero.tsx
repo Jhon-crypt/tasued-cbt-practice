bb905878fec80c2117b8704a9e1dd19e3383d13e
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-07-28 01:26:17 +0100

    Made some changes

--- app/components/hero/headingHero.tsx
+++ app/components/hero/headingHero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
-import {RegisterLink} from "@kinde-oss/kinde-auth-nextjs/server";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function HeadingHero(){
 
@@ -27,22 +27,19 @@ export default function HeadingHero(){
                                             </button>
                                         </Link>
                                         </div>
-                                        <div className="w-auto p-2.5">
-                                            <a href="#">
-                                            <button className="py-4 px-9 w-full font-semibold border border-gray-300 hover:border-gray-400 rounded-xl focus:ring focus:ring-gray-50 bg-transparent hover:bg-gray-100 transition ease-in-out duration-200" type="button">
-                                                <div className="flex flex-wrap justify-center items-center -m-1">
-                                                    <div className="w-auto p-1">
-                                                        <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M2.75 3.75C2.75 2.92157 3.42157 2.25 4.25 2.25H6.70943C7.03225 2.25 7.31886 2.45657 7.42094 2.76283L8.5443 6.13291C8.66233 6.48699 8.50203 6.87398 8.1682 7.0409L6.47525 7.88737C7.30194 9.72091 8.77909 11.1981 10.6126 12.0247L11.4591 10.3318C11.626 9.99796 12.013 9.83767 12.3671 9.9557L15.7372 11.0791C16.0434 11.1811 16.25 11.4677 16.25 11.7906V14.25C16.25 15.0784 15.5784 15.75 14.75 15.75H14C7.7868 15.75 2.75 10.7132 2.75 4.5V3.75Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                        </svg>
-                                                    </div>
-                                                    <div className="w-auto p-1">
-                                                        <RegisterLink>Signup</RegisterLink>
-                                                    </div>
-                                                </div>
-                                            </button>
-                                            </a>
-                                        </div>
+                                    <div className="w-auto p-2.5">
+                                        <details className="dropdown mb-32">
+                                            <summary  className="py-4 px-9 w-full font-semibold border border-gray-300 hover:border-gray-400 rounded-xl focus:ring focus:ring-gray-50 bg-transparent hover:bg-gray-100 transition ease-in-out duration-200">
+                                               
+                                                Create practice
+                                                
+                                            </summary>
+                                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                                                <li><LoginLink>Login</LoginLink></li>
+                                                <li><RegisterLink>Sign up</RegisterLink></li>
+                                            </ul>
+                                        </details>
+                                    </div>
                                     </div>
                                     <ul className="pb-10">
                                         <li className="mb-3 inline-flex items-center w-full font-medium leading-relaxed">
