27d43402354952c5d3fb3b5e29e4f9f0a982519d
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-08-08 14:29:39 +0100

    Added some changes

--- app/components/section/100levelPractice.tsx
+++ app/components/section/100levelPractice.tsx
@@ -96,7 +96,7 @@ export default function FirstLevelPracticeSection(){
                                                 <div className="stat-desc">{first_level.date_created}</div>
                                             </div>
 
-                                            <Link href="/practice/inputMatric">
+                                            <Link href={`/practice/inputMatric/knknjncdkn`}>
                                                 <div className="stat bg-primary text-white">
                                                     <div className="stat-value">Practice<br />Now</div>
                                                 </div>
--- app/practice/inputMatric/[id]/page.tsx
+++ app/practice/inputMatric/[id]/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/app/components/heading/heading"
 import Footer from "@/app/components/footer/footer"
 import InputMatricNumberForm from "@/app/components/forms/inputMatricNumberForm"
 
-export default function InputMatric(){
+export default function InputMatric({ params }: { params: { id: any } }){
 
     return (
 
@@ -10,7 +10,7 @@ export default function InputMatric(){
         
             <Header />
 
-                <InputMatricNumberForm />
+                <InputMatricNumberForm practice_id={params.id}/>
 
             <Footer />
         
