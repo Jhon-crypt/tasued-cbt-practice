6a1cc12a549e76fe9be191243d4b0a2ca7e2abb1
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-08-11 15:44:47 +0100

    Made some changes

--- app/components/section/cbtQuestionsSection.tsx
+++ app/components/section/cbtQuestionsSection.tsx
@@ -15,6 +15,8 @@ export default function CbtQuestions(props: { practice_id: any, student_id: any,
 
     const [cbt_questions, setCbtQuestions]: any = useState([])
 
+    const [loading2, setLoading2] = useState(false)
+
     useEffect(() => {
 
         async function getCbtQuestions(){
@@ -59,6 +61,27 @@ export default function CbtQuestions(props: { practice_id: any, student_id: any,
 
     }, [])
 
+    async function submitQuestions(event : any){
+
+        setLoading2(true)
+
+        event.preventDefault()
+
+        try{
+
+            const submitted_answers = {
+                Student_answer : String(event.target.choice.value),
+                Answer : String(event.target.ans.value)
+            }
+
+        }catch(error){
+
+            console.log(error)
+
+        }
+
+    }
+
     return (
 
         <>
@@ -79,46 +102,68 @@ export default function CbtQuestions(props: { practice_id: any, student_id: any,
                     
                         <div className="grid grid-flow-row auto-rows-max">
 
-                            {cbt_questions.map((questions: any) => (
+                            <form onSubmit={submitQuestions}>
 
-                                <div className="card w-96 bg-base-100 shadow-xl m-5">
-                                    <div className="card-body">
-                                        <p className="text-sm text-slate-400">Question 1</p>
-                                        <h2 className="card-title">{questions.question_name}</h2>
+                                {cbt_questions.map((questions: any) => (
 
-                                        <div className="form-control">
-                                            <label className="label cursor-pointer">
-                                                <span className="label-text">A. {questions.option_a}</span>
-                                                
-                                            </label>
-                                        </div>
-                                        <div className="form-control">
-                                            <label className="label cursor-pointer">
-                                                <span className="label-text">B. {questions.option_b}</span>
-                                                
-                                            </label>
-                                        </div>
+                                    <div className="card w-96 bg-base-100 shadow-xl m-5" key={questions.key}>
+                                        <div className="card-body">
+                                            <p className="text-sm text-slate-400">Question 1</p>
+                                            <h2 className="card-title">{questions.question_name}</h2>
 
-                                        <div className="form-control">
-                                            <label className="label cursor-pointer">
-                                                <span className="label-text">C. {questions.option_c}</span>
-                                                
-                                            </label>
-                                        </div>
+                                            <div className="form-control">
+                                                <label className="label cursor-pointer">
+                                                    <span className="label-text">A. {questions.option_a}</span>
 
-                                        <div className="form-control">
-                                            <label className="label cursor-pointer">
-                                                <span className="label-text">D. {questions.option_d}</span>
-                                                
+                                                </label>
+                                            </div>
+                                            <div className="form-control">
+                                                <label className="label cursor-pointer">
+                                                    <span className="label-text">B. {questions.option_b}</span>
+
+                                                </label>
+                                            </div>
+
+                                            <div className="form-control">
+                                                <label className="label cursor-pointer">
+                                                    <span className="label-text">C. {questions.option_c}</span>
+
+                                                </label>
+                                            </div>
+
+                                            <div className="form-control">
+                                                <label className="label cursor-pointer">
+                                                    <span className="label-text">D. {questions.option_d}</span>
+                                                </label>
+                                            </div>
+
+                                            <label className="block mb-5 w-full">
+                                                <select name="choice" id="level" className="w-full px-6 py-4 placeholder-gray-500 text-base text-gray-500 bg-white outline-none rounded-lg">
+                                                    <option>Answer</option>
+                                                    <option>option_a</option>
+                                                    <option>option_b</option>
+                                                    <option>option_c</option>
+                                                    <option>option_d</option>
+                                                </select>
                                             </label>
+
+                                            <input name="ans" type="hidden" value={questions.answer} />
+
                                         </div>
                                     </div>
-                                </div>
 
-                            ))}
 
+                                ))}
+
+                                <button className="mb-8 py-4 px-9 w-full text-white font-semibold border border-indigo-700 rounded-xl shadow-4xl focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200" type="submit">
+                                    Submit
+                                </button>
+
+                            </form>
 
                         </div>
+
+                        
                     
                     </>
 
