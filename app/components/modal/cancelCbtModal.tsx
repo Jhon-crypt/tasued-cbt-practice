8f9440f1b6b17cdc0dbd857d80477c90a28328e8
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-08-19 12:32:28 +0100

    Updated timer end utility and cbt cancel modal

--- app/components/modal/cancelCbtModal.tsx
+++ app/components/modal/cancelCbtModal.tsx
@@ -16,7 +16,7 @@ export default function CancelCbtModal(){
                         <div className="flex mt-5">
                             
                             <button className="font-bold text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-4 py-2 text-center mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-4">
-                                <Link href="/result">
+                                <Link href="/practice">
                                     Yes Cancel
                                 </Link>
                             </button>
--- app/components/utilities/handleTimerEnd.ts
+++ app/components/utilities/handleTimerEnd.ts
@@ -1,7 +1,7 @@
 const handleTimerEnd = () => {
     // This function will be called when the timer ends
     // You can perform any actions you want here
-    window.location.href= "http://localhost:3000/practice"
+    window.location.href= "https://tasued-cbt-practice.vercel.app/practice"
 };
 
 export default handleTimerEnd
