070f297aacb82814e29552347ee52f922f770713
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-08-22 13:32:53 +0100

    Made some changes

--- app/components/hero/callToActionHero.tsx
+++ app/components/hero/callToActionHero.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function CallToAction(){
 
     return (
@@ -10,7 +12,9 @@ export default function CallToAction(){
                         <h2 className="mb-5 text-6xl md:text-7xl font-bold font-heading text-center tracking-px-n leading-tight">Get started with the Tasued Cbt Practice</h2>
                         <p className="mb-7 text-lg text-gray-600 font-medium">Are you ready to ace your exams and tests? Practice with the Tasued CBT Practice site today!.</p>
                         <div className="mb-11 md:inline-block">
-                            <button className="py-4 px-6 w-full text-white font-semibold border border-indigo-700 rounded-xl shadow-4xl focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200" type="button">Practice Test - Exams Now</button>
+                            <Link href="/practice">
+                                <button className="py-4 px-6 w-full text-white font-semibold border border-indigo-700 rounded-xl shadow-4xl focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200" type="button">Practice Test - Exams Now</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
